874d9438ebe979f689325616ee7dc7fbcdff69e1
Chad Baxter <cbaxter@mail.umw.edu>
2018-11-01 15:10:56 -0500

    cleaned up tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-
 pub struct Spacebar {
     pub name: String,
     pub desc: String,
     pub spacebar: String
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }

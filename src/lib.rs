e359b5fc06f7ec496d8c5ae97fdd04fb8bd71629
Chad Baxter <cbaxter@mail.umw.edu>
2018-11-01 14:57:11 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+
+pub struct Spacebar {
+    pub name: String,
+    pub desc: String,
+    pub spacebar: String
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
